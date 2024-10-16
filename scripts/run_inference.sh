274569b71f8907f7e2b64496c2618953ef67ece3
ming-chieh-liu <eddie.m.c.liu@gmail.com>
2024-10-16 10:49:48 -0500

    delete use of singularity

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -1,23 +1,24 @@
 #!/bin/bash -l
 #SBATCH --job-name=ethos_infer
 #SBATCH --time=1-00:00:00
-#SBATCH --partition=defq
+#SBATCH --partition=gpuq
 #SBATCH --gres=gpu:8
-#SBATCH --output=ethos_infer.log
+#SBATCH --output=slurm/out/ethos_infer.log
+#SBATCH --error=slurm/out/ethos_infer.err
 
 gpu_num=8
 rep_start=1
 rep_stop=20
 model_variant="best_model.pt"
 
-dataset_dir="tokenized_datasets"
+datasets_dir="ethos/data/tokenized_datasets"
 test_name=$1
 shift 1
 additional_arg="$*"
 
 # Set dataset specific variables
-test_data="${dataset_dir}/mimic_test_timelines_p26758.hdf5"
-vocab="${dataset_dir}/mimic_vocab_t4367.pkl"
+test_data="${dataset_dir}/mimic_test_timelines_p32926.hdf5"
+vocab="${dataset_dir}/mimic_vocab_t4458.pkl"
 model_folder="mimic_layer_6_batch_32_do_0.3"
 
 # Set test_name specific variables
@@ -30,37 +31,24 @@ case $test_name in
   ;;
 esac
 
-script_body="
-cd /ethos/ethos_deploy
-pip install --no-deps --no-index --no-build-isolation --user -e .
-export PATH=\$HOME/.local/bin:\$PATH
-
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/compat/
-
-for i in \$(seq ${rep_start} ${rep_stop}); do
-    clear
-    echo [\${i}/${rep_stop}]: test=${test_name}, model=${model_variant}, dataset=${dataset}
-    ethos infer \\
-        --test ${test_name} \\
-        --model "out/${model_folder}/${model_variant}" \\
-        --data ${test_data} \\
-        --vocab ${vocab} \\
-        --model_name ${model_folder}_${model_variant%_*} \\
-        --n_jobs $(("$gpu_num" * 2)) \\
-        --n_gpus ${gpu_num} \\
-        ${additional_arg} \\
-        --suffix rep\${i} || exit 1
-done
-"
+source /home/${USER}/.bashrc
+mamba activate ethos
 
 clear
 echo "Running inference for ${dataset} ${test_name}"
 
-module load singularity
-singularity exec \
-  --contain \
-  --nv \
-  --writable-tmpfs \
-  --bind "$SCRATCH":/ethos \
-  ethos_latest.sif \
-  bash -c "${script_body}"
+# for test_name in "admission_mortality" "icu_readmission" "sofa" "drg" "readmission" "mortality"; do #"icu_mortality"
+for i in $(seq ${rep_start} ${rep_stop}); do
+    echo "[${i}/${rep_stop}]: test=${test_name}, model=${model_variant}, dataset=${dataset}"
+    ethos infer \
+        --test ${test_name} \
+        --model "out/${model_folder}/${model_variant}" \
+        --data ${test_data} \
+        --vocab ${vocab} \
+        --model_name ${model_folder}_${model_variant%_*} \
+        --n_jobs $((${gpu_num} * 2)) \
+        --n_gpus ${gpu_num} \
+        ${additional_arg} \
+        --suffix rep${i} || exit 1
+done
+# done
--- scripts/run_tokenization.sh
+++ scripts/run_tokenization.sh
@@ -1,25 +1,13 @@
 #!/bin/bash -l
 #SBATCH --job-name=ethos_tokenize
 #SBATCH --time=6:00:00
-#SBATCH --partition=defq
-#SBATCH --output=ethos_tokenize.log
+#SBATCH --partition=tier2q
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=160G
+#SBATCH --output=slurm/out/ethos_tokenize.log
 
-# you might need to create setup.py in the ethos_deploy directory
-
-script_body="
-cd /ethos/ethos_deploy
-pip install --no-deps --no-index --no-build-isolation --user -e .
-export PATH=\$HOME/.local/bin:\$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/compat/
-
+source /home/${USER}/.bashrc
+mamba activate ethos
 ethos tokenize- $*
-"
-
-module load singularity
-singularity exec \
-  --contain \
-  --nv \
-  --writable-tmpfs \
-  --bind "$SCRATCH":/ethos \
-  ethos_latest.sif \
-  bash -c "${script_body}"
